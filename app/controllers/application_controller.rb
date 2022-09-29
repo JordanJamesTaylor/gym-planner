5ed659a85c54ff9d4cbaa0f9e91e020c392b8a21
JordanJamesTaylor <jordjamestaylor@gmail.com>
2022-09-29 14:31:24 -0400

    React setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,10 @@ class ApplicationController < ActionController::API
     
     # Enable access to cookies hash
     include ActionController::Cookies
+    
+    def hello_world
+        session[:count] = (session[:count] || 0) + 1
+        render json: { count: session[:count] }
+      end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   
+  get '/hello', to: 'application#hello_world'
 
 end
